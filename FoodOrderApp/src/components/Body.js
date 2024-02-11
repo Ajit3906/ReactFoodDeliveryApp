49647e95c8365ec706ec75d8936cfa3812cf84f5
Ajit3906 <ajitpatare1@gmail.com>
2024-02-11 20:51:17 +0530

    added filter based on rating

--- FoodOrderApp/src/components/Body.js
+++ FoodOrderApp/src/components/Body.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import "./body.css";
 import resList from "../utils/mockData";
 
 const Body = () => {
+  const [restaurantList, setRestaurantList] = useState(resList);
   return (
     <div className="body">
-      {/* <div className="search">
-        <h3>Search</h3>
-      </div> */}
+      <div className="filter">
+        <button className="filter-btn" onClick={() => {
+          setRestaurantList(restaurantList.filter( res => res.info.avgRating > 4.0));
+        }}>Filter</button>
+      </div>
       <div className="res-container">
-        {resList?.map((res) => (
+        {restaurantList?.map((res) => (
           <RestaurantCard key={res.id} resData={res} />
         ))}
       </div>
