49eee3bd2b9d65232d4c8f6a16ebe1979585fbfe
Ajit3906 <ajitpatare1@gmail.com>
2024-03-13 18:41:10 +0530

    added Shimmer UI and login btn

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import ShimmerCards from "./ShimmerCards";
 import "./body.css";
-import resList from "../utils/mockData";
 
 const Body = () => {
-  const [restaurantList, setRestaurantList] = useState(resList);
-  return (
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [restaurantList]);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const restaurant = await data.json();
+    const filtered = restaurant?.data?.cards?.filter((element) => element.card.card.id === "top_brands_for_you");
+    setRestaurantList(filtered[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
+
+  return ( restaurantList?.length === 0 ? <ShimmerCards/> :
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={() => {
-          setRestaurantList(restaurantList.filter( res => res.info.avgRating > 4.0));
-        }}>Filter</button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setRestaurantList(
+              restaurantList?.filter((res) => res.info.avgRating > 4.0)
+            );
+          }}
+        >
+          Filter
+        </button>
       </div>
       <div className="res-container">
-        {restaurantList?.map((res) => (
-          <RestaurantCard key={res.id} resData={res} />
-        ))}
+          {restaurantList?.map((res) => (
+            <RestaurantCard key={res?.id} resData={res} />
+          ))}
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './header.css';
 import {LOGO_URL} from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+    const [btnName, setBtnName] = useState('Login');
+
     return (<div className="header">
         <div className="logo">
             <img src={LOGO_URL}></img>
@@ -11,8 +14,11 @@ const Header = () => {
             <ul>
                 <li><h5>Search</h5></li>
                 <li><h5>Offers</h5></li>
-                <li><h5>SignIn</h5></li>
                 <li><h5>Cart</h5></li>
+                <button className='login' onClick={() => {
+                    btnName === "Login" ?
+                    setBtnName('Logout') : setBtnName('Login');
+                }}>{btnName}</button>
             </ul>
         </div>
     </div>
